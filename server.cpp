6f445d54c2bb3afa50c5d8e2b6b1524bebbe64a0
on8t <64454213+on8t@users.noreply.github.com>
2024-10-16 06:23:07 +0000

    working recv

--- server.cpp
+++ server.cpp
@@ -1,13 +1,23 @@
-#include <iostream>
-#include <string>
-#include <exception>
-
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+#include <iostream>
+#include <string>
+#include <exception>
 #include <utility>
 #include <vector>
+
+#define DEBUG
+
+#ifdef DEBUG
+#define dbgout(expr) do { std::cout << expr << std::endl; } while (0)
+#else
+#define dbgout(expr)
+#endif
 
 class Socket {
 public:
@@ -20,10 +30,26 @@ public:
         }
     }
 
-    Socket(int socket_fd) : socket_fd(socket_fd) {}
+    // move constructor
+    Socket(Socket&& other) : socket_fd(other.socket_fd) {
+        dbgout("Socket(Socket&& other) with other.socket_fd = " << socket_fd);
+        other.socket_fd = -1;
+    }
+
+    Socket(int socket_fd) : socket_fd(socket_fd) {
+        dbgout("Socket(int) " << socket_fd);
+    }
 
     ~Socket() {
-        close(socket_fd);
+        dbgout("~Socket() " << socket_fd);
+        close();
+    }
+
+    void close() {
+        dbgout("close() " << socket_fd);
+        if (socket_fd == -1) return;
+        ::close(socket_fd);
+        socket_fd = -1;
     }
 
     void bind(std::string host, unsigned short port) {
@@ -47,15 +73,35 @@ public:
         struct sockaddr_in socket_addr;
         socklen_t addr_len = sizeof(socket_addr);
 
+        int conn_fd = ::accept(socket_fd, (struct sockaddr*)&socket_addr, &addr_len);
+        if (conn_fd == -1) {
+            throw std::exception();
+        }
+
         return {
-            Socket(::accept(socket_fd, (struct sockaddr*)&socket_addr, &addr_len)),
+            std::move(Socket(conn_fd)),
             Address(inet_ntoa(socket_addr.sin_addr), ntohs(socket_addr.sin_port))
         };
     }
 
-    std::vector<char> recv(int len, int flags = 0) {
+    std::vector<char> recv(size_t n, int flags = 0) {
         std::vector<char> buf;
-        buf.reserve(len);
+        buf.reserve(n);
+
+        size_t n_read = ::recv(socket_fd, buf.data(), n, flags);
+        dbgout("recv(): n_read = " << n_read);
+
+        if (n_read == -1) {
+            std::cout << strerror(errno) << std::endl;
+            std::cout << socket_fd << std::endl;
+            throw std::exception();
+        }
+
+        for (unsigned i = 0; i < n_read; i++) {
+            buf.push_back(buf.data()[i]);
+        }
+
+        return buf;
     }
 
     Address getsockname() {
@@ -89,6 +135,13 @@ int main (int argc, char *argv[]) {
     auto [ host, port ] = address;
 
     std::cout << host << ", " << port << std::endl;
+
+    std::vector<char> msg = connection.recv(32);
+
+    for (char c : msg) {
+        std::cout << c;
+    }
+    std::cout << std::endl;
 
     return 0;
 }
